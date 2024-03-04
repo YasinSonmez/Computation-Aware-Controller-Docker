f8849af0bc2c37fce37c60500a3596f2afa1bd0d
YasinSonmez <sonmezyasin6006@gmail.com>
2024-03-03 18:51:21 -0800

    Updated commands

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 APPTAINER_CACHEDIR=/tmp
 apptainer pull docker://ausar/controller_image
-apptainer run ausar/controller_image.sif
+apptainer run controller_image_latest.sif
 # apptainer run docker://ausar/controller_image
--- setup_scarab.sh
+++ setup_scarab.sh
@@ -1,17 +1,48 @@
-USERNAME=yasin
+USERNAME=yasinsonmez
+FOLDER_NAME=Computation-Aware-Controller-Docker
+WORKDIR=/global/scratch/users/$USERNAME/$FOLDER_NAME
 
-cd /home/$USERNAME && git clone https://github.com/YasinSonmez/scarab.git
-pip3 install -r /home/$USERNAME/scarab/bin/requirements.txt
+cd $WORKDIR && git clone https://github.com/YasinSonmez/scarab.git
+pip3 install -r $WORKDIR/scarab/bin/requirements.txt
 
-gdown 'https://drive.google.com/uc?id=1xD4zEEgg05CQEI9e5CQO4F5rRfJdfLZV' -O pinplay35.zip
-unzip pinplay35.zip
-rm pinplay35.zip
+# cd $WORKDIR && wget https://github.com/DynamoRIO/dynamorio/releases/download/cronbuild-9.0.19314/DynamoRIO-Linux-9.0.19314.tar.gz \
+#     && tar -xzvf DynamoRIO-Linux-9.0.19314.tar.gz \
+#     && rm DynamoRIO-Linux-9.0.19314.tar.gz
 
-export PIN_ROOT=/home/$USERNAME/pinplay-drdebug-3.5-pin-3.5-97503-gac534ca30-gcc-linux
+export DYNAMORIO_HOME=$WORKDIR/DynamoRIO-Linux-9.0.19314
+
+# cd $WORKDIR && gdown 'https://drive.google.com/uc?id=1xD4zEEgg05CQEI9e5CQO4F5rRfJdfLZV' -O pinplay35.zip
+# unzip pinplay35.zip
+# rm pinplay35.zip
+
+export PIN_ROOT=$WORKDIR/pinplay-drdebug-3.5-pin-3.5-97503-gac534ca30-gcc-linux
 export SCARAB_ENABLE_PT_MEMTRACE=1
 export SCARAB_ENABLE_MEMTRACE=1
-export LD_LIBRARY_PATH=/home/$USERNAME/pinplay-drdebug-3.5-pin-3.5-97503-gac534ca30-gcc-linux/extras/xed-intel64/lib
-export LD_LIBRARY_PATH=/home/$USERNAME/pinplay-drdebug-3.5-pin-3.5-97503-gac534ca30-gcc-linux/intel64/runtime/pincrt:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$WORKDIR/pinplay-drdebug-3.5-pin-3.5-97503-gac534ca30-gcc-linux/extras/xed-intel64/lib
+export LD_LIBRARY_PATH=$WORKDIR/pinplay-drdebug-3.5-pin-3.5-97503-gac534ca30-gcc-linux/intel64/runtime/pincrt:$LD_LIBRARY_PATH
 # export DYNAMORIO_HOME=/home/$USERNAME/DynamoRIO-Linux-9.0.19314
 
-cd /home/$USERNAME/scarab/src && make
+cd $WORKDIR/scarab/src && make
+
+# mkdir $WORKDIR/roi_trace
+# cp $WORKDIR/scarab/src/deps/dynamorio/clients/drcachesim/tests/burst_static.cpp $WORKDIR/roi_trace/
+
+# cd $WORKDIR/roi_trace && gdown 'https://drive.google.com/file/d/1izXMsz5DKzOTrH2Qejt_v0fzz43tE50z/view' -O CMakeLists.txt
+# cd $WORKDIR/roi_trace && mkdir build && cd build
+# cmake -DDynamoRIO_DIR=$DYNAMORIO_HOME/cmake ..
+# make
+
+
+cd $WORKDIR && git clone https://github.com/YasinSonmez/Inverted-Pendulum.git
+
+PROJECT_DIR=$WORKDIR/Inverted-Pendulum
+cd $PROJECT_DIR && git clone https://github.com/YasinSonmez/Scarab-Trace-and-Simulate-Script.git
+cd $PROJECT_DIR && mkdir build && cd build
+cmake -DDynamoRIO_DIR=$DYNAMORIO_HOME/cmake ..
+make
+
+# cd $WORKDIR && wget https://github.com/nicolapiccinelli/libmpc/archive/refs/tags/0.4.2.tar.gz \
+#     && tar -xzvf 0.4.2.tar.gz \
+#     && rm 0.4.2.tar.gz
+
+# cd $WORKDIR/libmpc-0.4.2 && sudo ./configure.sh --disable-test && mkdir build && cd build && cmake .. && make && sudo make install
