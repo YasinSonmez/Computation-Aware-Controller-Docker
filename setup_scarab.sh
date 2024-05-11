7bcb17d08977dff3f8549982a1ae1298ec3146ca
YasinSonmez <sonmezyasin6006@gmail.com>
2024-05-11 15:58:23 -0700

    Scarab repo change

--- setup_scarab.sh
+++ setup_scarab.sh
@@ -1,8 +1,9 @@
 USERNAME=yasinsonmez
-FOLDER_NAME=Computation-Aware-Controller-Docker
-WORKDIR=/global/scratch/users/$USERNAME/$FOLDER_NAME
+FOLDER_NAME=computation/Computation-Aware-Controller-Docker
+WORKDIR=/global/scratch/users/$USERNAME/$FOLDER_NAME/resources
 
-cd $WORKDIR && git clone https://github.com/YasinSonmez/scarab.git
+mkdir -p $WORKDIR
+cd $WORKDIR && git clone https://github.com/Litz-Lab/scarab.git
 pip3 install -r $WORKDIR/scarab/bin/requirements.txt
 
 cd $WORKDIR && wget https://github.com/DynamoRIO/dynamorio/releases/download/cronbuild-9.0.19314/DynamoRIO-Linux-9.0.19314.tar.gz \
@@ -11,19 +12,25 @@ cd $WORKDIR && wget https://github.com/DynamoRIO/dynamorio/releases/download/cro
 
 export DYNAMORIO_HOME=$WORKDIR/DynamoRIO-Linux-9.0.19314
 
-cd $WORKDIR && gdown 'https://drive.google.com/uc?id=1xD4zEEgg05CQEI9e5CQO4F5rRfJdfLZV' -O pinplay35.zip
-unzip pinplay35.zip
-rm pinplay35.zip
+# cd $WORKDIR && gdown 'https://drive.google.com/uc?id=1xD4zEEgg05CQEI9e5CQO4F5rRfJdfLZV' -O pinplay35.zip
+# unzip pinplay35.zip
+# rm pinplay35.zip
 
-export PIN_ROOT=$WORKDIR/pinplay-drdebug-3.5-pin-3.5-97503-gac534ca30-gcc-linux
+## Download pinplay 3.15
+cd $WORKDIR && gdown 'https://drive.google.com/uc?export=download&id=1KRrs0CfR67UGa_IcGDTzaL5zFBMds0ZS' -O pin-3.15-98253-gb56e429b1-gcc-linux.tar.gz \
+    && tar -xzvf pin-3.15-98253-gb56e429b1-gcc-linux.tar.gz \
+    && rm pin-3.15-98253-gb56e429b1-gcc-linux.tar.gz
+
+
+export PIN_ROOT=$WORKDIR/pin-3.15-98253-gb56e429b1-gcc-linux
 export SCARAB_ENABLE_PT_MEMTRACE=1
 export SCARAB_ENABLE_MEMTRACE=1
-export LD_LIBRARY_PATH=$WORKDIR/pinplay-drdebug-3.5-pin-3.5-97503-gac534ca30-gcc-linux/extras/xed-intel64/lib
-export LD_LIBRARY_PATH=$WORKDIR/pinplay-drdebug-3.5-pin-3.5-97503-gac534ca30-gcc-linux/intel64/runtime/pincrt:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$WORKDIR/pin-3.15-98253-gb56e429b1-gcc-linux/extras/xed-intel64/lib
+export LD_LIBRARY_PATH=$WORKDIR/pin-3.15-98253-gb56e429b1-gcc-linux/intel64/runtime/pincrt:$LD_LIBRARY_PATH
 
 cd $WORKDIR/scarab/src && make
 
-cd $WORKDIR && git clone https://github.com/YasinSonmez/Inverted-Pendulum.git
+# cd $WORKDIR && git clone https://github.com/YasinSonmez/Inverted-Pendulum.git
 
 # PROJECT_DIR=$WORKDIR/Inverted-Pendulum
 # cd $PROJECT_DIR && git clone https://github.com/YasinSonmez/Scarab-Trace-and-Simulate-Script.git
@@ -31,8 +38,8 @@ cd $WORKDIR && git clone https://github.com/YasinSonmez/Inverted-Pendulum.git
 # cmake -DDynamoRIO_DIR=$DYNAMORIO_HOME/cmake ..
 # make
 
-cd $WORKDIR && wget https://github.com/nicolapiccinelli/libmpc/archive/refs/tags/0.4.2.tar.gz \
-    && tar -xzvf 0.4.2.tar.gz \
-    && rm 0.4.2.tar.gz
+# cd $WORKDIR && wget https://github.com/nicolapiccinelli/libmpc/archive/refs/tags/0.4.2.tar.gz \
+#     && tar -xzvf 0.4.2.tar.gz \
+#     && rm 0.4.2.tar.gz
 
 # cd $WORKDIR/libmpc-0.4.2 && sudo ./configure.sh --disable-test && mkdir build && cd build && cmake .. && make && sudo make install
